4c66c191cee4d03e319f3ad86a8ce8ffd5b3c9a0
omora <omora@gs1uy.org>
2020-06-13 17:09:18 -0300

    First Commit

---
+++ build/src/models/progress/AssignmentProgress.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose = require("mongoose");
+const assignmentProgressSchema = new mongoose.Schema({
+    comment: {
+        type: String
+    }
+});
+exports.assignmentProgressSchema = assignmentProgressSchema;
+
+//# sourceMappingURL=AssignmentProgress.js.map
---
+++ build/src/security/passportJwtMiddleware.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const passportJwtMiddlewareFactory_1 = require("./passportJwtMiddlewareFactory");
+exports.default = passportJwtMiddlewareFactory_1.default('jwt');
+
+//# sourceMappingURL=passportJwtMiddleware.js.map
